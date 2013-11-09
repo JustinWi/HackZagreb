5459d63c7ec21c1a52483e4eaa2ee308ffc86d16
Justin Wilcox <justin@needtobounce.com>
2013-11-09 16:53:19 +0100

    Adding countdown clock

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+var clock;
+
+$(document).ready(function() {
+
+	// Grab the current date
+	var currentDate = new Date();
+
+	// Set some date in the future. In this case, it's always Jan 1
+	var futureDate = new Date();
+
+	futureDate.setUTCHours(19);
+	futureDate.setUTCMinutes(0);
+
+	// Calculate the difference in seconds between the future and current date
+	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+	// Instantiate a coutdown FlipClock
+	clock = $('.clock').FlipClock(diff, {
+		clockFace : 'HourlyCounter',
+		countdown : true
+	});
+});
+
 var fnames = new Array();
 var ftypes = new Array();
 fnames[0] = 'EMAIL';
